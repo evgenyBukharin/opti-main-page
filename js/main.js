437b8e2618436cd77f834dae9f244517bbaee187
Evgeniy Bukharin <buharinzxc123@gmail.com>
2023-08-08 14:29:33 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,49 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 732:
+/***/ (() => {
+
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+burger.addEventListener("click", () => {
+  burger.classList.toggle("burger--active");
+  menu.classList.toggle("menu-active");
+});
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
 
 ;// CONCATENATED MODULE: ./src/js/_vars.js
 /* harmony default export */ const _vars = ({
@@ -10513,12 +10556,17 @@ const heroSlider = new core(swiperEl, {
     pauseOnMouseEnter: true
   }
 });
+// EXTERNAL MODULE: ./src/js/components/burger.js
+var burger = __webpack_require__(732);
 ;// CONCATENATED MODULE: ./src/js/_components.js
+
 
 ;// CONCATENATED MODULE: ./src/js/main.js
 
 
 
+
+})();
 
 /******/ })()
 ;
